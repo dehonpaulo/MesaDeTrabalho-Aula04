b36e93a81553fae4cc3e85a4a8ce41de0f822463
Paulo Dehon <paulodehon@hotmail.com>
2021-10-30 17:52:14 -0300

    Homem feliz

---
+++ src/components/homemFeliz/HomemFeliz.js
@@ -0,0 +1,10 @@
+import homemFelizImg from './homem-feliz-dinheiro.jpg'
+
+function HomemFeliz() {
+    
+    return (
+        <img src={homemFelizImg} alt="Homem Feliz" />
+    )
+}
+
+export default HomemFeliz
